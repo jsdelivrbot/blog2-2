c967548dd8410383f47f1890bd2f34761e5c0323
vagrant <vagrant@homestead>
2018-05-26 20:49:59 +0000

    ok lesson 8

---
+++ resources/assets/js/components/Painel.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- v-bind para pegar a função defineCor definida no script lá embaixo   -->
+    <div v-bind:class="defineCor">
+        <div class="panel-heading">{{titulo}}</div>
+
+        <div class="panel-body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['titulo', 'cor'],
+        computed: {//computed function
+            defineCor: function () {
+                //atenção ao espaço em panel abaixo antes de fechar "
+                return "panel " + (this.cor || "panel-default");
+            }
+        }
+    }
+</script>
+<style>
+    .blue {
+        border-color: #0d4a96;
+    }
+    .blue > .panel-heading {
+        color: #f7f7f7;
+        background-color: #0d4a96;
+        border-color: #0d4a96;
+    }
+    .orange {
+        border-color: #e85e0d;
+    }
+    .orange > .panel-heading {
+        color: #f7f7f7;
+        background-color: #e85e0d;
+        border-color: #e85e0d;
+    }
+</style>
