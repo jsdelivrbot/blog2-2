fe0a0377f5771ce8a6d622dbc91b178e1fd859e2
vagrant <vagrant@homestead>
2018-05-26 21:03:14 +0000

    ok lesson 8

---
+++ resources/assets/js/components/Pagina.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['']
+    }
+</script>
